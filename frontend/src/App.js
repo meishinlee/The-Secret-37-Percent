ae842e6edc0eb438279dc608907a969986577bfd
Mei Shin Lee <53961253+meishinlee@users.noreply.github.com>
2022-02-19 02:26:04 -0500

    Set up React

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Home from './pages/home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      Hi
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+const Home = () => {
+  return (
+    <div>
+        Home
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </div>
+    
+  )
+}
+
+export default Home
