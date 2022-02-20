9c189dc8ee0d694e8a61075ff5be607c833ee976
Mei Shin Lee <53961253+meishinlee@users.noreply.github.com>
2022-02-20 10:41:46 -0500

    Added GRAPH LINK

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -16,6 +16,9 @@ import { useDispatch } from 'react-redux';
 import ShoppingListTable from '../components/shoppingtable/shoppingtable';
 import Autocomplete from '@mui/material/Autocomplete';
 import { setReduxItems } from '../reducers/itemsReducer';
+// import { Link } from "react-router-dom";
+// import Link from "next/link"
+
 
 const Home = () => {
 
@@ -147,7 +150,16 @@ const Home = () => {
                 <ShoppingListTable />
 
             </Box>
+            {/* C:\Users\miche\Documents\GitHub\TreeHacks2022\graphs\graph.html */}
+            {/* <a target="_blank" href="./../../graphs/graph.html">link text</a> */}
 
+
+ {/* <Link
+      to={{
+        pathname:
+            "https://hansbdejong.github.io/graph/graph.html",}}
+        target="_blank">
+ </Link> */}
         </div>
     )
 }
